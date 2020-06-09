a8f5a5ca184bee43cdcb48c48e24e5d507a7b465
sujiaojiao <lhbj_sujj@cnpc.com.cn>
2020-06-09 14:43:57 +0800

    babel使用

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+// var root = document.getElementById('root');
+// // import './index.scss';
+// root.innerHTML = ('<div >Helloworld</div>');
+// console.log('nihoa fefwefsssssssfseef');
+// import "@babesl/polyfill";
+const arr = [
+	new Promise(()=>{}),
+	new Promise(()=>{})
+];
+arr.map(item=>{
+	console.log(item);
+});
